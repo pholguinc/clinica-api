b9f90b3a12074276a030e20159c3974a79bedf73
Pedro Holguín <49991514+pholguinc@users.noreply.github.com>
2023-07-13 13:39:49 -0500

    Update app.controller.ts hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get()
-  getData('hello') {
+  @Get('hello')
+  getData() {
     const data = {
       message: 'Hello world json',
     };
